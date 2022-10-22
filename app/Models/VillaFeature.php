7da17e0bf20837d65342898eb41488780d85045c
a-dev-id <3xasov@gmail.com>
2022-10-22 14:27:02 +0800

    first commit

---
+++ app/Models/VillaFeature.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VillaFeature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'villa_id',
+        'title',
+        'icon',
+        'status',
+    ];
+
+    public function villa()
+    {
+        return $this->belongsTo(Villa::class, 'villa_id', 'id');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+require __DIR__ . '/auth.php';
+
+// ADMIN
+Route::middleware(['auth', 'verified'])->prefix('panel/admin')->group(function () {
+    Route::resource('dashboard', App\Http\Controllers\Admin\DashboardController::class);
+    Route::resource('villa', App\Http\Controllers\Admin\VillaController::class);
+    Route::resource('villa-setting', App\Http\Controllers\Admin\VillaSettingController::class);
+    Route::resource('villa-feature', App\Http\Controllers\Admin\VillaFeatureController::class);
+    Route::resource('villa-image', App\Http\Controllers\Admin\VillaImageController::class);
+    Route::resource('feature', App\Http\Controllers\Admin\FeatureController::class);
+});
